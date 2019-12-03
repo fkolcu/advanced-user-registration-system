94b0f76231c0811daf8fcea333ba4a6b0399c355
furkan <furkankolcu@outlook.com.tr>
2019-12-03 14:56:48 +0300

    write scenarios for both context api and domain

--- features/bootstrap/DomainContext.php
+++ features/bootstrap/DomainContext.php
@@ -7,7 +7,7 @@ use Behat\Gherkin\Node\TableNode;
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext implements Context
+class DomainContext implements Context
 {
     /**
      * Initializes context.
